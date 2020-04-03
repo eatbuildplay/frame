dc55fdfadbf5663de10ed7575972da1ccab04b9e
Casey Milne <joel@goldhat.ca>
2020-04-02 22:52:08 -0500

    adding post list and filter

--- src/post_lists/PostList.php
+++ src/post_lists/PostList.php
@@ -4,25 +4,129 @@ namespace Frame;
 
 class PostList {
 
+  public function __construct() {
+
+    $this->initShortcode();
+    $this->addScripts();
+    $this->initAjaxHooks();
+
+  }
+
   public function getPostType() {
-    return 'lesson';
+    return 'phrase';
   }
 
   public function initShortcode() {
     require_once( FRAME_PATH . 'src/post_lists/PostListShortcode.php' );
-    new PostListShortcode();  
+    new PostListShortcode();
   }
 
-  public function fetchPosts() {
+  public function initAjaxHooks() {
+    add_action('wp_ajax_frame_post_list_load', [$this, 'ajaxListLoad']);
+    add_action('wp_ajax_nopriv_frame_post_list_load', [$this, 'ajaxListLoad']);
+  }
 
-    $posts = get_posts(
-      'numberposts' => -1,
-      'post_type'   => $this->getPostType()
+  public function fetchPosts( $metaquery, $taxquery ) {
+
+    $postType = $this->getPostType();
+    $posts = get_posts( array(
+        'numberposts' => -1,
+        'post_type'   => $postType,
+        'meta_query'  => $metaquery,
+        'tax_query'   => $taxquery
+      )
     );
 
     return $posts;
 
   }
 
+  public function addScripts() {
+
+    wp_enqueue_style(
+      'frame-post-list-css',
+      FRAME_URL . 'src/post_lists/assets/post_list.css',
+      array(),
+      '1.0.0',
+      'all'
+    );
+
+    wp_enqueue_script(
+      'frame-post-list-js',
+      FRAME_URL . 'src/post_lists/assets/post_list.js',
+      array( 'jquery' ),
+      '1.0.0',
+      true
+    );
+
+    wp_localize_script(
+      'frame-post-list-js',
+      'frame',
+      [
+        'ajaxurl' => admin_url( 'admin-ajax.php' )
+      ]
+    );
+
+  }
+
+  public function ajaxListLoad() {
+
+    // get filter values
+    $filters = $_POST['filters'];
+
+    // setup metaquery
+    $metaquery = [];
+    $metaquery['relation'] = 'AND';
+
+    /*
+    if( $filters['propertyType'] ) {
+
+      $metaquery[] = array(
+        'key'	  	=> 'property_type',
+        'value'	  => $filters['propertyType'],
+        'compare' => '=',
+      );
+
+    }
+    */
+    $taxquery = [];
+    if( $filters['topic'] ) {
+      $taxquery = array(
+        array(
+          'taxonomy' => 'topic',
+          'field'    => 'term_id',
+          'terms'    => $filters['topic'],
+          'include_children' => false
+        )
+      );
+    }
+
+    $posts = $this->fetchPosts( $metaquery, $taxquery );
+
+    // setup template
+    $template = new Template();
+    $template->path = 'src/post_lists/templates/';
+    $template->name = 'post-list-item';
+
+    // load list template
+    if( !empty( $posts )) :
+      foreach( $posts as $post ) :
+        $template->data = array(
+          'post' => $post,
+        );
+        $content .= $template->get();
+      endforeach;
+    endif;
+
+    // send response and exit
+    $response = [
+      'posts'       => $posts,
+      'content'     => $content,
+      'status'      => 'success'
+    ];
+    print json_encode( $response );
+    wp_die();
+
+  }
 
 }
---
+++ src/post_lists/assets/post_list.js
@@ -0,0 +1,39 @@
+(function($) {
+
+  function loadPostList() {
+
+    console.log('loadPostList called...')
+
+    var filterPropertyType = $('#filter_topic').val()
+
+    // do ajax call to get new filtered posts
+    data = {
+      action: 'frame_post_list_load',
+      filters: {
+        propertyType: filterPropertyType
+      }
+    }
+    $.post( frame.ajaxurl, data, function( response ) {
+
+      response = JSON.parse( response )
+
+      if ( response.status == 'success' ) {
+
+        // replace content
+        $('.frame-post-list-canvas').html( response.content )
+
+      } else {
+
+      }
+    });
+
+  }
+
+  // init load
+  loadPostList();
+
+  $('#filter_topic').on('change', function() {
+    loadPostList();
+  })
+
+})( jQuery );
