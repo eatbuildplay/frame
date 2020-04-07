de49f797a4e5bdca42849d51fd813fefc00cc0e2
Casey Milne <joel@goldhat.ca>
2020-04-07 17:35:31 -0500

    exercise tabs

--- components/lesson/templates/lesson-single-tabs.php
+++ components/lesson/templates/lesson-single-tabs.php
@@ -1,9 +1,9 @@
 <div class="lesson-single-tabs">
 
-  <h2>Lesson Tabs</h2>
+  <h2>Lesson Exercises</h2>
 
   <ul>
-    <li>Word Scan</li>
+    <li class="active">Word Scan</li>
     <li>Flash Cards</li>
     <li>Word Selection</li>
   </ul>
